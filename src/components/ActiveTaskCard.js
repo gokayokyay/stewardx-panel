40b94b6167841f228d4c6caa1ee621094b676d21
Gökay Okyay <gokayokyay@hotmail.com>
2021-04-04 22:27:09 +0000

    (ActiveTaskCard) abort

--- src/components/ActiveTaskCard.js
+++ src/components/ActiveTaskCard.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { abortTask } from '../api';
 import useInterval from '../hooks/useInterval';
 import OrangeButton from './OrangeButton';
 import WideCard from './WideCard';
@@ -9,15 +10,19 @@ export default function ActiveTaskCard({
   last_exec = new Date(),
   ...props
 }) {
+  const id = props.id;
   const [secsPassed, setSecsPassed] = useState((new Date() - new Date(last_exec)).valueOf());
   useInterval(() => {
     setSecsPassed(secsPassed + 1);
   }, 1000);
+  const onAbortClick = () => {
+    abortTask(id);
+  };
   return (
     <WideCard
       ButtonComponent={
         <div className="flex-1 flex justify-center items-center pb-4 px-2">
-          <OrangeButton >
+          <OrangeButton onClick={onAbortClick}>
             Abort task
           </OrangeButton>
          </div>
